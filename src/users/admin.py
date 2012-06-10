fa2762d97c7beabf3d299fbd7787126fb9988b9a
amrnegm <amrnegm.01@gmail.com>
2012-06-10 17:13:43 +0200

    modified admin

--- src/users/admin.py
+++ src/users/admin.py
@@ -6,7 +6,7 @@ from django.contrib import admin
 class NotificationAdmin(admin.ModelAdmin):
     model = Notification
     list_display = ('title', 'category', 'which_day', 'for_whom')
-    list_editable = ('category', 'which_day', 'for_whom')
+    list_editable = ('category', 'hajj_type', 'which_day', 'for_whom')
     list_filter = ('for_whom', 'category', 'which_day')
 
 #admin.site.register(Country)
